e27aa4b8ab365feb4e9249ec7354b441523607bb
sofia <sofia@teascade.net>
2025-07-22 14:19:28 +0300

    Fix pointer-casting

--- reid/src/codegen.rs
+++ reid/src/codegen.rs
@@ -1030,18 +1030,22 @@ impl mir::Expression {
             }
             mir::ExprKind::CastTo(expression, type_kind) => {
                 let val = expression.codegen(scope, state)?;
-                let cast_instr = val
-                    .1
-                    .get_type(scope.type_values, scope.types)
-                    .cast_instruction(
-                        val.instr(),
-                        &type_kind.get_type(scope.type_values, scope.types),
-                    )
-                    .unwrap();
-                Some(StackValue(
-                    val.0.derive(scope.block.build(cast_instr).unwrap()),
-                    type_kind.clone(),
-                ))
+                let instr =
+                    if let (TypeKind::UserPtr(_), TypeKind::UserPtr(_)) = (&val.1, type_kind) {
+                        val.0.instr()
+                    } else {
+                        let cast_instr = val
+                            .1
+                            .get_type(scope.type_values, scope.types)
+                            .cast_instruction(
+                                val.instr(),
+                                &type_kind.get_type(scope.type_values, scope.types),
+                            )
+                            .unwrap();
+                        scope.block.build(cast_instr).unwrap()
+                    };
+
+                Some(StackValue(val.0.derive(instr), type_kind.clone()))
             }
         };
         if let Some(value) = &value {
