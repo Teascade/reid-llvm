bc59b6f5759f20febe7781a887899f5f901bf19c
sofia <sofia@teascade.net>
2025-07-29 17:48:45 +0300

    Start adding LSP implementation

---
+++ reid-lsp/client/src/extension.ts
@@ -0,0 +1,69 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import * as path from 'path';
+import { workspace, ExtensionContext, window } from 'vscode';
+
+import {
+	Executable,
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient/node';
+
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+	const traceOutputChannel = window.createOutputChannel("Reid Language Server trace");
+	const command = process.env.SERVER_PATH || "reid-language-server";
+
+
+	const run: Executable = {
+		command,
+		options: {
+			env: {
+				...process.env,
+				RUST_LOG: "debug",
+			}
+		}
+	};
+
+	const serverOptions: ServerOptions = {
+		run,
+		debug: run,
+	};
+
+	// Options to control the language client
+	const clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: 'reid' }],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'reid-lsp',
+		'Reid Language Server',
+		serverOptions,
+		clientOptions
+	);
+
+	client.info("hello");
+
+	// Start the client. This will also launch the server
+	client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
